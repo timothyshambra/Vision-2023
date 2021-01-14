dfdcd0437ccb92a6b55cdb055783fae9695ae390
Timothy Shambra <timothy.shambra@state.ca.gov>
2021-01-14 12:13:47 -0800

    Update get involved section

--- components/get-involved.js
+++ components/get-involved.js
@@ -2,13 +2,13 @@
 class GetInvolved extends window.HTMLElement {
     connectedCallback () {
       const { classPrefix, classList } = this.dataset;
-      this.innerHTML = `<div class="section-understated p-5">
+      this.innerHTML = `<div class="section-understated p-5" id="get-involved-sec">
       <div class="container">
           <div class="row justify-content-center">
-              <div class="col">
+              <div class="col-md-8 mx-auto">
                   <div class="text-center">
-                      <h2 class="h2 p-4 m-b color-gray-dark">How to Get Involved</h2>
-                      <p style="margin: 0 auto; width:90%;max-width:550px;">Click on the buttons below to
+                      <h2 class="h2 p-4 m-b">How to Get Involved</h2>
+                      <p class="lead">Click on the buttons below to
                           share
                           your thoughts about the <a
                               href="https://ca-code-works.github.io/Vision-2023/challenge/">challenges</a> we
@@ -16,10 +16,9 @@ class GetInvolved extends window.HTMLElement {
                           first, including what we might be missing and any additional insight that would help
                           inspire our work.</p>
                       <div class="group p-t-md p-b-sm">
-                          <a href="#" class="btn btn-highlight rounded-50 p-x-md m-r-sm" role="button">Get involved</a>
-                          <a href="#" class="btn btn-outline-primary btn-hover p-x-md rounded-50 m-l-sm"
-                              role="button">Give
-                              feedback</a>
+                          <a href="#" class="btn btn-highlight btn-lg rounded-50 p-x-md m-r-sm" role="button">Get involved</a>
+                          <a href="https://ca-code-works.github.io/Vision-2023/challenge/" class="btn btn-outline-primary btn-lg btn-hover p-x-md rounded-50 m-l-sm"
+                              role="button">Explore challenges</a>
                       </div>
                   </div>
               </div>

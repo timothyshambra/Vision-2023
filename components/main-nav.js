6dec50d2e28f0e07f8bcd8cad5e431a01920a425
konstantin <61205810+koryfey@users.noreply.github.com>
2021-01-14 13:27:58 -0800

    added more padding to the accordion, updated main nav

--- components/main-nav.js
+++ components/main-nav.js
@@ -5,14 +5,18 @@ class MainNav extends window.HTMLElement {
       this.innerHTML = `<nav id="navigation" class="main-navigation singlelevel auto-highlight no-icons">
       <ul id="nav_list" class="top-level-nav">
       <li class="nav-item">
+      <a href="/principles/" class="first-level-link">Guiding Principles</a>
+  </li>
+      <li class="nav-item">
           <a href="/goals/" class="first-level-link">Our Goals</a>
       </li>
       <li class="nav-item">
           <a href="/next/" class="first-level-link">What's Next</a>
       </li>
       <li class="nav-item">
-          <a href="/principles/" class="first-level-link">Guiding Principles</a>
-      </li>
+      <a href="https://ca-code-works.github.io/Vision-2023/challenge/" target="_blank" class="first-level-link">Explore Challenges</a>
+  </li>
+
   </ul></nav>`;
     }
   

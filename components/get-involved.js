d5ddc1c3edc764b4a78eb98a94d209012978242a
konstantin <61205810+koryfey@users.noreply.github.com>
2021-01-14 14:51:44 -0800

    changed get involved button text to provide input

--- components/get-involved.js
+++ components/get-involved.js
@@ -11,12 +11,12 @@ class GetInvolved extends window.HTMLElement {
                       <p class="lead">We are actively looking for people to contribute. Here’s how you can help:</p>
                       <ol style="font-weight:300">
                       <li>Review the current <a href="https://ca-code-works.github.io/Vision-2023/challenge/">challenges</a> we are looking to tackle.</li>
-                      <li>Provide your input on challenges and your interest in being part of a challenge team.</li>
+                      <li><a href=""https://forms.gle/zSbFssCE4YCWYiTB8">Provide your input</a> on challenges and your interest in being part of a challenge team.</li>
                       <li>Our team will follow up in the coming weeks to better understand how you can help</li>
                       </ol>
                       <div class="text-center">
                       <div class="group p-t-md p-b-sm">
-                          <a href="#" class="btn btn-highlight btn-lg rounded-50 p-x-md m-r-sm" role="button">Get involved</a>
+                          <a href="https://forms.gle/zSbFssCE4YCWYiTB8" class="btn btn-highlight btn-lg rounded-50 p-x-md m-r-sm" role="button">Provide input</a>
                           <a href="https://ca-code-works.github.io/Vision-2023/challenge/" class="btn btn-outline-primary btn-lg btn-hover p-x-md rounded-50 m-l-sm"
                               role="button">Explore challenges</a>
 

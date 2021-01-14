c4f99de868288cb326525d188ba93f2d1e7c4e1a
konstantin <61205810+koryfey@users.noreply.github.com>
2021-01-14 12:38:05 -0800

    updated get involved section agaain

--- components/get-involved.js
+++ components/get-involved.js
@@ -6,20 +6,20 @@ class GetInvolved extends window.HTMLElement {
       <div class="container">
           <div class="row justify-content-center">
               <div class="col-md-8 mx-auto">
-                  <div class="text-center">
-                      <h2 class="h2 p-4 m-b">How to Get Involved</h2>
-                      <p class="lead">Click on the buttons below to
-                          share
-                          your thoughts about the <a
-                              href="https://ca-code-works.github.io/Vision-2023/challenge/">challenges</a> we
-                          plan to address
-                          first, including what we might be missing and any additional insight that would help
-                          inspire our work.</p>
+                 
+                      <h2 class="text-center h2 p-4 m-b">How to Get Involved</h2>
+                      <p class="lead">We are actively looking for people to contribute. Here’s how you can help:</p>
+                      <ol stayle="font-weight:300">
+                      <li>Review the current <a href="https://ca-code-works.github.io/Vision-2023/challenge/">challenges</a> we are looking to tackle.</li>
+                      <li>Provide your input on challenges and your interest in being part of a challenge team.</li>
+                      <li>Our team will follow up in the coming weeks to better understand how you can help</li>
+                      </ol>
+                      <div class="text-center">
                       <div class="group p-t-md p-b-sm">
                           <a href="#" class="btn btn-highlight btn-lg rounded-50 p-x-md m-r-sm" role="button">Get involved</a>
                           <a href="https://ca-code-works.github.io/Vision-2023/challenge/" class="btn btn-outline-primary btn-lg btn-hover p-x-md rounded-50 m-l-sm"
                               role="button">Explore challenges</a>
-                              
+
                       </div>
                   </div>
               </div>

205f93b3cf9d29156a057da01589550b321f9695
Dan Hon <daniel.hon@state.ca.gov>
2021-01-15 10:01:04 -0800

    Update get-involved.js (#23)
    Tweaked get involved/share feedback language.
    
    Drafting off the "feedback"  language from alpha.ca.gov vs "provide input" for consistency.
    
    Remove rollover effect from btn
    
    Co-authored-by: timothyshambra <timothy.shambra@gmail.com>

--- components/get-involved.js
+++ components/get-involved.js
@@ -7,16 +7,16 @@ class GetInvolved extends window.HTMLElement {
           <div class="row justify-content-center">
               <div class="col-md-9 mx-auto">
                  
-                      <h2 class="text-center h2 pb-4 m-b">How to Get Involved</h2>
-                      <p class="lead">We are actively looking for people to contribute. Here’s how you can help:</p>
+                      <h2 class="text-center h2 pb-4 m-b">How to Share Feedback and Get Involved</h2>
+                      <p class="lead">We are actively looking for feedback and people to get involved in Vision 2023. Here’s how you can help:</p>
                       <ol style="font-weight:300">
-                      <li>Review the current <a href="https://ca-code-works.github.io/Vision-2023/challenge/">challenges</a> we are looking to tackle.</li>
-                      <li><a href="https://forms.gle/zSbFssCE4YCWYiTB8">Provide your input</a> on challenges and your interest in being part of a challenge team.</li>
-                      <li>Our team will follow up in the coming weeks to better understand how you can help</li>
+                      <li>Read the Vision 2023 <a href="https://ca-code-works.github.io/Vision-2023/challenge/">goals and challenges</a>.</li>
+                      <li><a href="https://forms.gle/zSbFssCE4YCWYiTB8">Share your feedback</a> and your interest in being part of a challenge team.</li>
+                      <li>Our team will follow up in the coming weeks to better understand how you can help.</li>
                       </ol>
                       <div class="text-center">
                       <div class="btn-row p-t-sm">
-                          <a href="https://forms.gle/zSbFssCE4YCWYiTB8" class="btn btn-highlight btn-lg rounded-50 p-x-md m-r-sm" role="button">Provide input</a>
+                          <a href="https://forms.gle/zSbFssCE4YCWYiTB8" class="btn btn-highlight btn-lg rounded-50 p-x-md m-r-sm" role="button">Share feedback</a>
                           <a href="https://ca-code-works.github.io/Vision-2023/challenge/" class="btn btn-outline-primary btn-lg p-x-md rounded-50 m-l-sm"
                               role="button">Explore challenges</a>
                       </div>

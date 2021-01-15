2f2be480a68609736fa73751251784c4881b67c6
Timothy Shambra <timothy.shambra@state.ca.gov>
2021-01-15 11:12:35 -0800

    Add web links to appropriate items per GO document #14

--- components/main-nav.js
+++ components/main-nav.js
@@ -4,24 +4,25 @@ class MainNav extends window.HTMLElement {
       const { classPrefix, classList } = this.dataset;
       this.innerHTML = `<nav id="navigation" class="main-navigation singlelevel auto-highlight no-icons" aria-owns="nav-icon3">
       <ul id="nav_list" class="top-level-nav">
-      <li class="nav-item">
-      <a href="/" class="first-level-link">Home</a>
-  </li>
-      <li class="nav-item">
-      <a href="/principles/" class="first-level-link">Our Principles</a>
-  </li>
-      <li class="nav-item">
-          <a href="/goals/" class="first-level-link">Our Goals</a>
-      </li>
-      <li class="nav-item">
-      <a href="https://ca-code-works.github.io/Vision-2023/challenge/" class="first-level-link">Explore Challenges</a>
-  </li>
-      <li class="nav-item">
-          <a href="/next/" class="first-level-link">What's Next</a>
-      </li>
-      
-  </ul></nav>`;
+        <li class="nav-item">
+            <a href="/" class="first-level-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="/vision/" class="first-level-link">Our Vision</a>
+        </li>
+        <li class="nav-item">
+            <a href="/principles/" class="first-level-link">Principles</a>
+        </li>
+        <li class="nav-item">
+            <a href="/goals/" class="first-level-link">Goals</a>
+        </li>
+        <li class="nav-item">
+            <a href="https://ca-code-works.github.io/Vision-2023/challenge/" class="first-level-link">Challenges</a>
+        </li>
+        <li class="nav-item">
+            <a href="/next/" class="first-level-link">What's Next</a>
+        </li>    
+      </ul></nav>`;
     }
-  
   }
   window.customElements.define('main-nav', MainNav);

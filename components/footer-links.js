c5d537c9b9cd70bb21806f751f8fc27062f059a0
Timothy Shambra <timothy.shambra@state.ca.gov>
2021-01-14 18:11:54 -0800

    Update footer nav links #13

--- components/footer-links.js
+++ components/footer-links.js
@@ -4,19 +4,17 @@ class FooterLinks extends window.HTMLElement {
       const { classPrefix, classList } = this.dataset;
       this.innerHTML = `<div class="row"><div class="col-sm">
       <ul class="list-none">
-      <li class="m-y"><a href="/vision/">Our Vision</a></li>
-          <li class="m-y"><a href="/process/">Our Process</a></li>
-          <li class="m-y"><a href="/strategy.html">Our strategy for getting to vision 2023</a></li>
-          
+        <li class="m-y"><a href="/vision/">Our Vision</a></li>
+        <li class="m-y"><a href="/process/">Our Process</a></li>
+        <li class="m-y"><a href="/acknowledgements.html">Acknowledgements</a></li>
       </ul>
   </div>
   <div class="col-sm">
       <ul class="list-none">
-      <li class="m-y"><a href="/acknowledgements.html">Acknowledgements</a></li>
-          <li class="m-y"><a href="/our-thanks.html">Our thanks to</a></li>
-          <li class="m-y"><a href="/benefits-from.html">Vision 2023 benefits from the work of</a></li>
+        <li class="m-y"><a href="/our-thanks.html">Our thanks to</a></li>
+        <li class="m-y"><a href="/references.html">References</a></li>
       </ul>
-  </div></div>`;
+  </div>`;
     }
   
   }

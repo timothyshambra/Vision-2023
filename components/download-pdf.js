18a745029438f97cb95741e96945fffa27f13785
Timothy Shambra <timothy.shambra@state.ca.gov>
2021-01-15 10:07:24 -0800

    Remove rollover btn effect #22

--- components/download-pdf.js
+++ components/download-pdf.js
@@ -1,4 +1,4 @@
-// This is just the arrow icons
+// Download PDF Component
 class DownloadPDF extends window.HTMLElement {
     connectedCallback () {
       const { classPrefix, classList } = this.dataset;
@@ -7,7 +7,7 @@ class DownloadPDF extends window.HTMLElement {
           <div class="row justify-content-center">
               <div class="col">
                   <div class="text-center">
-                      <a href="#" class="btn btn-outline-light btn-hover rounded-50" role="button"><span
+                      <a href="#" class="btn btn-outline-light rounded-50" role="button"><span
                               class="ca-gov-icon-file-pdf"></span> Download Vision 2023 PDF document</a>
                   </div>
               </div>

473ea7cba5280614cc4d7e2d5897241c2f4ed8c7
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-01-14 18:16:43 -0800

    added vision page and added our  vision link to the footer links

--- components/footer-links.js
+++ components/footer-links.js
@@ -4,13 +4,15 @@ class FooterLinks extends window.HTMLElement {
       const { classPrefix, classList } = this.dataset;
       this.innerHTML = `<div class="row"><div class="col-sm">
       <ul class="list-none">
+      <li class="m-y"><a href="/vision/">Our Vision</a></li>
           <li class="m-y"><a href="/process/">Our Process</a></li>
           <li class="m-y"><a href="/strategy.html">Our strategy for getting to vision 2023</a></li>
-          <li class="m-y"><a href="/acknowledgements.html">Acknowledgements</a></li>
+          
       </ul>
   </div>
   <div class="col-sm">
       <ul class="list-none">
+      <li class="m-y"><a href="/acknowledgements.html">Acknowledgements</a></li>
           <li class="m-y"><a href="/our-thanks.html">Our thanks to</a></li>
           <li class="m-y"><a href="/benefits-from.html">Vision 2023 benefits from the work of</a></li>
       </ul>

80ab3748624eead21502369fdc49e498061e1db3
timothyshambra <timothy.shambra@gmail.com>
2021-01-15 18:06:06 -0800

    Include download PDF button in util nav, and update PDF doc. (#42)
    * Update index.html (#39)
    
    Updating meta description from temp text.
    
    * Update typo
    
    * Address typo in pdf
    
    * Update utility link to include vision 2023 pdf
    
    Co-authored-by: Dan Hon <daniel.hon@state.ca.gov>
    Co-authored-by: Timothy Shambra <timothy.shambra@state.ca.gov>

--- components/utility-links.js
+++ components/utility-links.js
@@ -2,7 +2,7 @@
 class UtilityLinks extends window.HTMLElement {
     connectedCallback () {
       const { classPrefix, classList } = this.dataset;
-      this.innerHTML = `<a href="/translations.html"><span class="ca-gov-icon-globe" aria-hidden="true"></span> Translate</a>
+      this.innerHTML = `<a href="/pdf/Vision-2023-California-Technology-Strategic-Plan.pdf"><span class="ca-gov-icon-download" aria-hidden="true"></span> Download Vision 2023 PDF</a> <a href="/translations.html"><span class="ca-gov-icon-globe" aria-hidden="true"></span> Translate</a>
   <button class="btn btn-xs btn-primary" data-toggle="collapse" data-target="#siteSettings"
       aria-expanded="false" aria-controls="siteSettings"><span class="ca-gov-icon-gear"
           aria-hidden="true"></span> Settings</button>`;
